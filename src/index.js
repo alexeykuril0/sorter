1420ea6ccd1c516b723105748f9b99c9643d1999
Alexey <43179292+alexeykuril0@users.noreply.github.com>
2018-09-17 17:30:02 +0300

    1.3.0

--- src/index.js
+++ src/index.js
@@ -1,30 +1,48 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.array = []; 
+this.compatator = (a,b) => a-b; 
   }
 
   add(element) {
     // your implementation
+    this.array.push(element); 
   }
 
   at(index) {
     // your implementation
+    return this.array[index]; 
   }
 
   get length() {
     // your implementation
+    return this.array.length; 
+ 
   }
 
   toArray() {
     // your implementation
+    return this.array; 
   }
 
   sort(indices) {
     // your implementation
+    indices.sort(); 
+if(indices.length == 1) return this.array; 
+let arr = []; 
+for(let i = 0; i<indices.length; i++){ 
+arr.push(this.array[indices[i]]); 
+} 
+arr.sort(this.compatator); 
+for(let i = 0; i<indices.length; i++){ 
+this.array[indices[i]] = arr[i]; 
+} 
   }
 
   setComparator(compareFunction) {
     // your implementation
+    this.compatator = compareFunction; 
   }
 }
 
